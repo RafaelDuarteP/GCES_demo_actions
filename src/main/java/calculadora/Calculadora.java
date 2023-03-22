6fda8b0b01419a667e946991a7dd56c2dbc344ad
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-03-22 16:09:51 -0300

    Revert "Add error on dividir"
    This reverts commit 363fa568dc2d96b1a1f61fc38a0c655ef72825e1.

--- src/main/java/calculadora/Calculadora.java
+++ src/main/java/calculadora/Calculadora.java
@@ -70,7 +70,7 @@ public class Calculadora {
         if (b == 0) {
             throw new ArithmeticException("Divisão por zero");
         }
-        return b / a;
+        return a / b;
     }
     
 }
